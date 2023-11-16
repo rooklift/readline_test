d4cee95d6e9b2c611e7dfd29af39c5344d292d2a
rooklift <16438795+rooklift@users.noreply.github.com>
2023-11-16 16:54:33 +0000

    Stuff

--- testprog.go
+++ testprog.go
@@ -28,15 +28,15 @@ func output(n int) {
 
 	arr := make([]byte, n + 1)
 
-	for i := 0; i < n; i++ {
-		if i == n - 1 {
-			arr[i] = 'E'			// E for END - the last character except the newline
-		} else {
-			arr[i] = 'c'			// c for character
-		}
+	for i := 0; i < len(arr); i++ {
+		arr[i] = '.'
 	}
-	arr[n] = '\n'
+	arr[len(arr) - 1] = '\n'
 
-	os.Stdout.Write(arr[:])
+	if len(arr) > 3 {
+		copy(arr[len(arr) - 4 : len(arr) - 1], "END")
+	}
+
+	os.Stdout.Write(arr)
 
 }
